796bbe1801c87fd830b1cbabb8b3b59982be0a8a
grclaeys <grclaeys@gmail.com>
2015-10-01 13:08:49 -0700

    added settings
    settings dont work and need to be fixed, same as about menu

--- app/src/main/java/com/example/claegar16/locayytionscommands/MainActivity.java
+++ app/src/main/java/com/example/claegar16/locayytionscommands/MainActivity.java
@@ -1,9 +1,26 @@
 package com.example.claegar16.locayytionscommands;
 
+import android.media.audiofx.Equalizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,27 +28,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment main = new MainFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, main);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.addToBackStack(null);
+        ft.commit();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
+        switch (id) {
+            case R.id.action_done:
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                Bundle args = new Bundle();
+                args.putString("Menu", "You pressed done button.");
+                Fragment detail = new TextFragment();
+                detail.setArguments(args);
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, detail).commit();
+
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(),"Settings Clicked",Toast.LENGTH_SHORT).show();
+                Intent ayyIntent = new Intent(this, Equalizer.Settings.class);
+                startActivity(ayyIntent);
+                return true;
+            case R.id.action_status:
+                Toast.makeText(getApplicationContext(),"Status Clicked",Toast.LENGTH_SHORT).show();
+                Intent launchStatus = new Intent(this, About.class);
+                startActivity(launchStatus);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1234 && resultCode == RESULT_OK) {
+            String voice_text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0);
+            Toast.makeText(getApplicationContext(),voice_text,Toast.LENGTH_LONG).show();
 
-        return super.onOptionsItemSelected(item);
+        }
     }
 }
