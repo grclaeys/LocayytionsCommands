d3626156e787d3302b284c8c554b648144ca8eb6
xXJohnCenaXx <thomas@potatoplanet.org>
2015-10-07 14:17:32 -0700

    makes things better
    ayy

--- app/src/main/java/com/example/claegar16/locayytionscommands/MainActivity.java
+++ app/src/main/java/com/example/claegar16/locayytionscommands/MainActivity.java
@@ -7,19 +7,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.content.IntentSender;
 import android.location.Location;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.LatLng;
+
 
 
 public class MainActivity extends AppCompatActivity implements
@@ -27,46 +23,16 @@ public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.OnConnectionFailedListener,
         LocationListener {
 
-    public double currentLatitude;
-    public double currentLongitude;
-
-
-//    private double myLat;
-//    private double myLong;
-
-//    protected LocationRequest mLocationRequest;
-//    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
-//    public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
-//            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
-//    protected GoogleApiClient mGoogleApiClient;
-//    protected static final String TAG = "location-updates-sample";
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
-    private GoogleApiClient mGoogleApiClient;
-    public static final String TAG = MapsActivity.class.getSimpleName();
-    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-    private LocationRequest LocationRequest;
-
-
-//    public double getMyLat(){
-//        return myLat;
-//    }
-//
-//    public double getMyLong(){
-//        return myLong;
-//    }
-//
-//    public void setMyLat(double myLat) {
-//        myLat = this.myLat;
-//    }
-//
-//    public void setMyLong(double myLong) {
-//        myLong = this.myLong;
-//    }
-
+        public double currentLatitude;
+        public double currentLongitude;
+        private GoogleApiClient mGoogleApiClient;
+        public static final String TAG = MapsActivity.class.getSimpleName();
+        private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+        private LocationRequest LocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -78,10 +44,7 @@ public class MainActivity extends AppCompatActivity implements
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(10 * 1000)        // 10 seconds, in milliseconds
                 .setFastestInterval(1 * 1000); // 1 second, in milliseconds
-
-
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -100,17 +63,14 @@ public class MainActivity extends AppCompatActivity implements
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
     private void handleNewLocation(Location location) {
-
         Log.d(TAG, location.toString());
         Log.d(TAG, location.toString());
         currentLatitude = location.getLatitude();
         currentLongitude = location.getLongitude();
-        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
     }
 
     public void onConnected(Bundle bundle) {
@@ -123,7 +83,6 @@ public class MainActivity extends AppCompatActivity implements
             handleNewLocation(location);
         }
     }
-
 
     @Override
     public void onConnectionSuspended(int i) {
@@ -149,9 +108,6 @@ public class MainActivity extends AppCompatActivity implements
 
         handleNewLocation(location);
     }
-
-
-
 
     public void storeLocation(View view) {
 
