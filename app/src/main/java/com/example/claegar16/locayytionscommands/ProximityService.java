d38d5fedd9914ea2ffe6c2f54fb8084237c33a46
grclaeys <grclaeys@gmail.com>
2015-10-22 12:27:57 -0700

    Enjoy my broken code
    this is all your fault. damn i cant type

--- app/src/main/java/com/example/claegar16/locayytionscommands/ProximityService.java
+++ app/src/main/java/com/example/claegar16/locayytionscommands/ProximityService.java
@@ -3,11 +3,17 @@ package com.example.claegar16.locayytionscommands;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 
 
@@ -18,7 +24,8 @@ import com.google.android.gms.maps.GoogleMap;
  * TODO: Customize class - update intent actions, extra parameters and static
  * helper methods.
  */
-public class ProximityService extends IntentService implements Runnable{
+public class ProximityService extends IntentService implements Runnable, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        LocationListener{
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     private static final String ACTION_FOO = "com.example.claegar16.locayytionscommands.action.FOO";
@@ -27,7 +34,7 @@ public class ProximityService extends IntentService implements Runnable{
     // TODO: Rename parameters
     private static final String EXTRA_PARAM1 = "com.example.claegar16.locayytionscommands.extra.PARAM1";
     private static final String EXTRA_PARAM2 = "com.example.claegar16.locayytionscommands.extra.PARAM2";
-    
+
 
     public double currentLatitude;
     public double currentLongitude;
@@ -95,6 +102,13 @@ public class ProximityService extends IntentService implements Runnable{
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect(); //makes it work
+
         LocationRequest = LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(10 * 1000)        // 10 seconds, in milliseconds
@@ -112,6 +126,45 @@ public class ProximityService extends IntentService implements Runnable{
 
             }
         }).start();
+    }
+
+    private void handleNewLocation(Location location) {
+
+
+        Log.d(TAG, location.toString());
+        Log.d(TAG, location.toString());
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+        myLocation = location;
+    }
+
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Location services connected.");
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(location == null){
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, LocationRequest, this);
+        }
+        else {
+            handleNewLocation(location);
+        }
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
     }
 
 
@@ -146,5 +199,10 @@ public class ProximityService extends IntentService implements Runnable{
     private void handleActionBaz(String param1, String param2) {
         // TODO: Handle action Baz
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Location services suspended. Please reconnect.");
     }
 }
